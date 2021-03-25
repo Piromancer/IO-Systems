5e53d8203b9ea8692635ee03a80cc1b7ffda0601
Piromancer <Den2000_09@mail.ru>
2021-03-25 20:02:31 +0000

    Fixed core dumps

--- Lab1.c
+++ Lab1.c
@@ -1,196 +1,209 @@
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/version.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
-	
-#define MAX_VALUES 100
-#define BUFSIZE 1000
-	
+#include <linux/init.h>
+ 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Rozhnov Denis P3400\nTarasenko Daniil P3400");
-MODULE_DESCRIPTION("Lab 1 Variant 5");
-MODULE_VERSION("0.1");
-	
-static int custom_open(struct inode*, struct file*);
-static int custom_close(struct inode*, struct file*);
-static ssize_t custom_write(struct file*, const char __user*, size_t, loff_t*);
-static ssize_t custom_read(struct file*, char __user*, size_t, loff_t*);
-static ssize_t proc_read(struct file*, char __user*, size_t, loff_t*);
-static void read(char *buf, int * const length);
-	
-static const char d_name[] = "var5";
-static const char d_greeting[] = "[VAR5]: ";
-static struct proc_dir_entry* entry;
-static int values[MAX_VALUES];
-static int values_idx;
-static dev_t d_number;
-static struct cdev c_dev;
+MODULE_AUTHOR("Rozhnov Denis P3400 && Tarasenko Daniil P3400");
+MODULE_DESCRIPTION("Lab 1");
+
+void handle_command(char* input);
+const char* CHANGE_FILENAME =  "/change_filename ";
+static dev_t first;
+static struct cdev c_dev; 
 static struct class *cl;
-static int dev_major;
-static int dev_minor;
-	
-static struct file_operations fops =
+static struct proc_dir_entry* entry;
+
+
+struct res_vector
 {
-	.owner = THIS_MODULE,
-	.read = proc_read,
+	int *data;
+	size_t size;
+	size_t idx;
 };
-	
-static struct file_operations dops =
+
+struct res_vector rv;
+
+struct res_vector rv_init(void)
 {
-	.owner = THIS_MODULE,
-	.open = custom_open,
-	.release = custom_close,
-	.read = custom_read,
-	.write = custom_write
-};
-	
+	return (struct res_vector) {
+		.data = vmalloc(sizeof(int)),
+		.size = 1,
+		.idx = 0,
+	};
+}
+
+void rv_append(size_t value, struct res_vector *r_vector)
+{
+	if (r_vector->idx == r_vector->size - 1)
+	{
+		r_vector->size *= 2;
+		int* n_data = (int*)vmalloc(r_vector->size * sizeof(int));
+		memcpy(n_data, r_vector->data, r_vector->idx * sizeof(int));
+		vfree(r_vector->data);
+		r_vector->data = n_data;
+	}
+	r_vector->data[r_vector->idx++] = value;
+}
+
+int count_letters(const char* string) {
+	char* cur = string;
+	int res = 0;
+	while (*cur != '\0' ) {
+		if ((*cur >= 'A' && *cur <= 'Z') || 
+		     (*cur >= 'a' && *cur <= 'z')) {
+			res++;
+		}
+		cur++;
+	}
+	return res;
+}
+
+
+static ssize_t proc_write(struct file *file, const char __user * ubuf, size_t count, loff_t* ppos) 
+{
+	printk(KERN_DEBUG "Attempting to write proc file");
+  	return -1;
+}
+
+
+static ssize_t proc_read(struct file *file, char __user * ubuf, size_t count, loff_t* ppos) 
+{
+  	char sarr[512];
+  	int written = 0;
+  	int i = 0;
+  	size_t len;
+
+  	for (i = 0; i < rv.idx; i++)
+    		written += snprintf(&sarr[written], 512 - written, "%d word had %d letters\n", i+1, rv.data[i]);
+  	sarr[written] = 0;
+
+       	len = strlen(sarr);
+  	if (*ppos > 0 || count < len) return 0;
+  	if (copy_to_user(ubuf, sarr, len) != 0) return -EFAULT;
+  	*ppos = len;
+  	return len;
+}
+
 static int custom_open(struct inode *i, struct file *f)
 {
-	return 0;
+  	return 0;
 }
-	
+
 static int custom_close(struct inode *i, struct file *f)
 {
-	return 0;
+  	return 0;
 }
-	
-static ssize_t custom_write(struct file *f, const char __user *ubuf, size_t len, loff_t *off)
-{
-	char buf[BUFSIZE];
-	
-    if (*off > 0 || len > BUFSIZE)
-    {
-	    printk(KERN_INFO "[VAR5 (%d %d)]: error occour, buffer to long\n", dev_major, dev_minor);
-	    return -EFAULT;
-    }
-	    
-    if (copy_from_user(buf, ubuf, len))
-	    return -EFAULT;
-	    
-    int i;
-    int num = 0;
-    for (i = 0; i < len; i++)
-    {
-	    char ch = buf[i];
-	    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-	        num++;
-	}
-	
-    if (values_idx >= MAX_VALUES)
-	    return -EFAULT;
-	
-    values[values_idx++] = num;
-	
-    int str_len = strlen(buf);
-    *off = str_len;
-    return str_len;
-}
-	
+
 static ssize_t custom_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-	char str[MAX_VALUES * sizeof(int) + sizeof(d_greeting)];
-	int length;
-	read(str, &length);
-	
-	printk(KERN_INFO "[VAR5 %d %d]: %s", dev_major, dev_minor, str);
+	int i = 0;
+  	for(i = 0; i < rv.idx; i++)
+		printk(KERN_DEBUG "%d word had %d letters\n", i+1, rv.data[i]);
 	return 0;
 }
-	
-static ssize_t proc_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+
+static ssize_t custom_write(struct file *f, const char __user *buf,  size_t len, loff_t *off)
 {
-	char str[MAX_VALUES * sizeof(int) + sizeof(d_greeting)];
-	int length;
-	read(str, &length);
-	
-	if (*off > 0 || len < length)
-	{
-	    printk(KERN_INFO "[VAR5 (%d %d)]: FAILED to read proc file: invalid offset", dev_major, dev_minor);
-	    return 0;
+	char* user_input = (char*)vmalloc(len * sizeof(char));
+	memcpy(user_input, buf, len * sizeof(char));
+	user_input[len] = 0;
+	if (*user_input == '/') {
+		handle_command(user_input);
+	} else {
+		int res = count_letters(user_input);
+		rv_append(res, &rv);
 	}
-	
-	if (copy_to_user(buf, str, length) != 0)
-	{
-	    printk(KERN_INFO "[VAR5 (%d %d)]: FAILED to read proc file: failed copy to user buffer", dev_major, dev_minor);
-	    return -EFAULT;
+	*off += len;
+  	return len;
+}
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = proc_read,
+	.write = proc_write,
+};
+
+static struct file_operations device_operations =
+{
+  	.owner = THIS_MODULE,
+  	.open = custom_open,
+  	.release = custom_close,
+  	.read = custom_read,
+  	.write = custom_write
+};
+
+void handle_command(char* input){
+	int i = 0;
+	for (; i < 17; ++i) {
+		if (*input != CHANGE_FILENAME[i]) return;
+		input++;
 	}
-	
-	*off = length;
-	return length;
-}
-	
-static void read(char *buf, int * const length)
-{
-	char *str_ptr = buf;
-	str_ptr += sprintf(buf, d_greeting);
-	
-	int i;
-	for (i = 0; i < values_idx; i++)
-	{
-	    str_ptr += sprintf(str_ptr, "%d ", values[i]);
+
+	while (*input == ' ') {
+		input++;
 	}
-	*(str_ptr++) = '\n';
-	*(str_ptr++) = '\0';
-	
-	*length = str_ptr - buf;
-}
-	
-static char *set_devnode(struct device *dev, umode_t *mode)
-{
-	    if (mode != NULL)
-	        *mode = 0666;
-	    return NULL;
-}
-	
-static int __init init(void)
-{
-	if (alloc_chrdev_region(&d_number, 0, 1, d_name) != 0)
-	    return -EFAULT;
-	
-	if ((cl = class_create(THIS_MODULE, d_name)) == NULL)
-	    goto dev_region_destroy;
-	
-	cl->devnode = set_devnode;
-	
-	if (device_create(cl, NULL, d_number, NULL, d_name) == NULL)
-	    goto class_destroy;
-	
-	cdev_init(&c_dev, &dops);
-	
-	if (cdev_add(&c_dev, d_number, 1) < 0)
-	    goto dev_destroy;
-	
-	if ((entry = proc_create(d_name, 0444, NULL, &fops)) == NULL)
-	    goto dev_destroy;
-	
-	dev_major = MAJOR(d_number);
-	dev_minor = MINOR(d_number);
-	
-	printk(KERN_INFO "[VAR5 (%d %d)]: initialized\n", dev_major, dev_minor);
-	return 0;
-	
-	dev_destroy:
-	device_destroy(cl, d_number);
-	class_destroy:
-	class_destroy(cl);
-	dev_region_destroy:
-	unregister_chrdev_region(d_number, 1);
-	return -EFAULT;
-}
-	
-static void __exit exit(void)
-{
-	cdev_del(&c_dev);
-	device_destroy(cl, d_number);
-	class_destroy(cl);
-	unregister_chrdev_region(d_number, 1);
+
 	proc_remove(entry);
-	printk(KERN_INFO "[VAR5 (%d %d)]: exit\n", dev_major, dev_minor);
+
+	char* cur = input;
+	while(*cur != '\n') {
+		cur++;
+	} 
+	*cur = 0;
+	entry = proc_create(input, 0444, NULL, &fops);
+	printk(KERN_INFO "created new file '%s'\n", input);		
 }
+
+ 
+static int __init ch_drv_init(void)
+{
+    	printk(KERN_INFO "Initialization\n");
 	
-	module_init(init);
-	module_exit(exit);
+	rv = rv_init();
+
+    	entry = proc_create("var5", 0444, NULL, &fops);
+    	if (alloc_chrdev_region(&first, 0, 1, "ch_dev") < 0) return -1;
+    	if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
+	{
+		unregister_chrdev_region(first, 1);
+		return -1;
+	}
+    	if (device_create(cl, NULL, first, NULL, "var5") == NULL)
+    	{
+		class_destroy(cl);
+		unregister_chrdev_region(first, 1);
+		return -1;
+    	}
+    	cdev_init(&c_dev, &device_operations);
+    	if (cdev_add(&c_dev, first, 1) == -1)
+	{
+		device_destroy(cl, first);
+		class_destroy(cl);
+		unregister_chrdev_region(first, 1);
+		return -1;
+	}
+    	return 0;
+}
+ 
+static void __exit ch_drv_exit(void)
+{
+    	proc_remove(entry);
+    	cdev_del(&c_dev);
+    	device_destroy(cl, first);
+    	class_destroy(cl);
+    	unregister_chrdev_region(first, 1);
+    	printk(KERN_INFO "Exit\n");
+}
+ 
+module_init(ch_drv_init);
+module_exit(ch_drv_exit);
